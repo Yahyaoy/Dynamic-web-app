01a7973c8b834c4a98d9e9f4ed4a973fa63f4825
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-05-13 19:37:13 +0300

    make controller and router

---
+++ views/index.view.php
@@ -0,0 +1,11 @@
+<?php require('partial/header.php') ?>
+<?php require('partial/nav.php') ?>
+<?php require('partial/banner.php') ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <p>Hello. Welcome to the index page.</p>
+        </div>
+    </main>
+
+<?php require('partial/foot.php') ?>
