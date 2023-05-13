87d1c045de94df85cb238cea06cd3fc24abd32df
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-05-13 11:10:47 +0300

    no-change

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-require "functions.php";
-
-$heading = "Dashboard";
-require "views/index.view.php";
-
+require 'functions.php';
+dd($_SERVER);
+$uri = $_SERVER['REQUEST_URI'];
+if($uri === '/dynamic-web-app/controllers/index.php'){
+    require 'controllers/index.php';
+}else if($uri === '/about'){
+    require 'controllers/about.php';
+}else if($uri === '/contact'){
+    require 'controllers/contact.php';
+}
