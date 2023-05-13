ea51dbc05132e07fb9131b9f755f28cb5786b4c3
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-05-13 10:21:11 +0300

    Initial commit

---
+++ views/partial/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en" style="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!--    <link rel="stylesheet" href="tailwinds.css">-->
+    <title>Dynamic web app</title>
+</head>
+<body style="h-full">
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
