d7f0eef84ac88ff6cc826556812c3cf637207d3b
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-05-14 08:58:16 +0300

    Episode 18

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 require "functions.php";
+require "router.php";
+//
+// connect to our MySql database.
+        require 'Database.php';
+        $db = new Database();
+        $posts =  $db->query("select * from posts ")->fetchAll(PDO::FETCH_ASSOC);
 
-$heading = "Dashboard";
-require "views/index.view.php";
+        dd($posts);
+//        foreach ($posts as $post){
+//            echo "<li>". $post['title'] . "</li>";
+//        }
 
+
+
+//class Person{
+//    public $name;
+//    public $age;
+//    public function breathe(){
+//        echo $this->name . ' is breathing';
+//    }
+//}
+//
+//$person = new Person();
+//$person->name = 'yahya khalid';
+//$person->age = 25;
+//dd($person->breathe());
